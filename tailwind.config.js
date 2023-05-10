79b0362a93c63966857a437f17de1e11377feca8
Daniil Zhuravlev <nakrovati@outlook.com>
2023-05-11 03:49:23 +0400

    style: update colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,16 +9,17 @@ export default {
   theme: {
     colors: {
       surface: {
-        1: "#F8FAFC",
+        1: "#eff8ff",
       },
       white: "#FFF",
-      green: "#649263",
+      green: "#38862a",
       blue: {
-        100: "#F1F6F9",
-        200: "#E3ECF3",
-        500: "#5590B4",
-        600: "#457B9D",
-        800: "#2C4E63",
+        100: "#daecff",
+        200: "#bddfff",
+        500: "#358dfc",
+        600: "#1e6ef2",
+        700: "#1758de",
+        800: "#1947b4",
       },
       red: { 500: "#E63946", 700: "rgb(218 5 5)" },
     },
