a325e61ffbb6931663ba6e855f7cebac5dcf8a5a
Daniil Zhuravlev <nakrovati@outlook.com>
2023-05-11 03:49:53 +0400

    chore: remove default layout

--- pages/index.vue
+++ pages/index.vue
@@ -5,8 +5,8 @@ useHead({
 </script>
 
 <template>
-  <main class="mt-8">
-    <div class="mx-auto max-w-[640px] flex-shrink px-4 lg:max-w-[920px]">
+  <main>
+    <div class="mx-auto max-w-[640px] flex-shrink px-4 pt-8 lg:max-w-[920px]">
       <URLShortener />
     </div>
   </main>
