63041807200a016de24909e8c5d2412540402b1c
Daniil <nakrovati@outlook.com>
2023-05-30 04:13:42 +0200

    feat: add Intro section

---
+++ components/URLShortener/URLShortenerIntro.vue
@@ -0,0 +1,10 @@
+<template>
+  <section class="mb-2">
+    <h1 class="mb-8 text-4xl font-bold dark:text-white-dark sm:text-5xl">
+      Shorten your URL
+    </h1>
+    <p class="dark:text-white-dark">
+      Type the URL you want to shorten in the field below
+    </p>
+  </section>
+</template>
