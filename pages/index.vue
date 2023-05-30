5300870440452a0ed894da8754cc32db07cc29cb
Daniil <nakrovati@outlook.com>
2023-05-30 03:48:08 +0200

    chore: correct grammatical error

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
 useServerSeoMeta({
   title: "Cutu - URL shortener",
   description:
-    "CutU: Simplify your URLs effortlessly. Transform long, complex web addresses into short, shareable links with our user-friendly interface. Perfect for social media, emails, and any situation that demands concise URLs. Streamline your online presence with CutU today!",
+    "Cutu: Simplify your URLs effortlessly. Transform long, complex web addresses into short, shareable links with our user-friendly interface. Perfect for social media, emails, and any situation that demands concise URLs. Streamline your online presence with CutU today!",
 });
 </script>
 
